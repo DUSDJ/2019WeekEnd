0f586801ca0b549faec0c69473a15889838eda12
myorang <eoeh92@naver.com>
2019-12-07 05:47:04 +0900

    Singleton Generic

---
+++ Assets/Main/Scripts/Generic/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+public class Singleton<T> :  MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instnace;
+    public static T Instance
+    {
+        get
+        {
+            if(_instnace == null)
+                _instnace = FindObjectOfType<T>() as T;
+                if (_instnace == null)
+                    Debug.LogError("There's no active " + typeof(T) + " in this scene.");
+
+            return _instnace;
+        }
+        private set { }
+    }
+}
