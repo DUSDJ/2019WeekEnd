e803e736b35663a0314c2df7c3c48ef4d08a33fd
myorang <eoeh92@naver.com>
2019-12-07 21:48:01 +0900

    Counsel

---
+++ Assets/Main/Scripts/Counsel/CounselModel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class CounselModel
+{
+    public Character Hero;
+
+    public string Title;
+    public string Body;
+    public string[] Selections;
+}
+
+
+
