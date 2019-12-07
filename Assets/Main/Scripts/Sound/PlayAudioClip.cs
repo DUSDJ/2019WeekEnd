c51ea0f625985993a66d51b972191f1f7becad4a
myorang <eoeh92@naver.com>
2019-12-08 02:39:58 +0900

    SoundButton

---
+++ Assets/Main/Scripts/Sound/PlayAudioClip.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Button))]
+public class PlayAudioClip : MonoBehaviour
+{
+    public string audioCilpFileName;
+    public Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(Play);
+    }
+
+    public void Play()
+    {
+        SoundManager.Instance.PlaySFX(audioCilpFileName);
+    }
+}
